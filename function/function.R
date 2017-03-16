080a632c604f46aa04b60007d25a6aeb72bf61ee
jid24 <jid24@pitt.edu>
2017-03-16 13:55:39 -0400

    2

--- function/function.R
+++ function/function.R
@@ -1,7 +1,5 @@
-# test
-print("hello world")
-
-
-f1<- function(){
-  
+yearlist<-c(2010,2011,2012)
+for(n in yearlist){
+  msales<-mean(sales[year==n],na.rm = T)
+  print(paste("on year",n,"mean sales is", msales))
 }
